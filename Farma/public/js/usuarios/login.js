d51f6c226fac3d348e7f0f580471facfe1da705b
LuisGomez335 <98978488+LuisGomez335@users.noreply.github.com>
2022-02-03 16:26:46 -0500

    login

--- Farma/public/js/usuarios/login.js
+++ Farma/public/js/usuarios/login.js
@@ -1,5 +1,21 @@
 function loginUsuario(){
-    alert("funciona")
+    
+    $.ajax({
+        type:"POST",
+        data:$('#frmLogin').serialize(),
+        url:"procesos/usuarios/login/loginUsuario.php",
+        
+    success:function(respuesta){
+        respuesta = respuesta.trim();
+
+        if (respuesta == 1){
+            window.location.href= "vistas/inicio.php"
+        } else {
+            swal.fire(":(","Error al Ingresar" + respuesta,"Error");
+        }
+    }
+
+    });
     
     return false; 
     
