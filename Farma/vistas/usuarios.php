d54cef57bf8e0182bfc2e4d5935a0f08a0991c9e
LuisGomez335 <98978488+LuisGomez335@users.noreply.github.com>
2022-02-04 08:44:17 -0500

    agregar usuario parte1

--- Farma/vistas/usuarios.php
+++ Farma/vistas/usuarios.php
@@ -12,9 +12,9 @@ if (isset($_SESSION['usuario']) &&
 <div class="container">
   <div class="card border-0 shadow my-5">
     <div class="card-body p-5">
-      <h1 class="fw-light">Usuarios</h1>
+      <h1 class="fw-light">Administrar Usuarios</h1>
       <p class="lead"> 
-        <button class= "btn btn-primary"  data-bs-toggle="modal" data-bs-target="#modalAgregarUsuarios" >
+        <button class= "btn btn-primary"  data-toggle="modal" data-target="#modalAgregarUsuarios">
           Agregar Usuario
         </button>
         <hr>
@@ -29,7 +29,9 @@ if (isset($_SESSION['usuario']) &&
 
 include "usuarios/modalAgregar.php";
 include "footer.php";
-
+?>
+  <script src="../public/js/usuarios/usuarios.js"></script>
+<?php
 } else {
   header("location:../index.html");
 }
--- Farma/vistas/usuarios/modalAgregar.php
+++ Farma/vistas/usuarios/modalAgregar.php
@@ -1,39 +1,90 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</head>
-<body>
 
-<div  >
+<!-- Modal -->
+<form id="frmAgregarUsuario" method="POST" onsubmit="return agregarNuevoUsuario()">
 
-  <!-- Modal -->
-  <div class="modal fade" id="modalAgregarUsuarios" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content" >
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Modal Header</h4>
+<div class="modal fade" id="modalAgregarUsuarios" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Crear Usuario</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+       <div class="row" >
+          <div class = "col-sm-4">
+              <label for="paterno"  >Primer Apellido</label>
+              <input type="text" class="form-control" id="paterno" name="paterno" required >
+          </div>
+          <div class = "col-sm-4">
+              <label for="materno"  >Segundo Apellido</label>
+              <input type="text" class="form-control" id="materno" name="materno" required >
+          </div>
+          <div class = "col-sm-4">
+              <label for="nombre"  >Nombre</label>
+              <input type="text" class="form-control" id="nombre" name="nombre" required>
+          </div>
+       </div>
+
+       <div class="row" >
+          <div class = "col-sm-4">
+              <label for="fechaNacimiento"  >Fecha de Nacimiento</label>
+              <input type="Date" class="form-control" id="fechaNacimiento" name="fechaNacimiento" required>
+          </div>
+          <div class = "col-sm-4">
+              <label for="sexo"  >Genero</label>
+              <select class="form-control" id="sexo" name="sexo" required >
+              <option value =""></option>  
+              <option value ="F">Femenino</option>
+                <option value ="M">Masculino</option>
+              </select>
+          </div>
+          <div class = "col-sm-4">
+              <label for="telefono"  >Telefono</label>
+              <input type="text" class="form-control" id="telefono" name="telefono" required>
+          </div>
+       </div>
+
+       <div class="row" >
+          <div class = "col-sm-4">
+              <label for="correo"  >Correo</label>
+              <input type="mail" class="form-control" id="correo" name="correo" required >
+          </div>
+          <div class = "col-sm-4">
+              <label for="usuario"  >Usuario</label>
+              <input type="text" class="form-control" id="usuario" name="usuario" required >
+          </div>
+          <div class = "col-sm-4">
+              <label for="password"  >Contraseña</label>
+              <input type="password" class="form-control" id="password" name="password" required>
+          </div>
+       </div>
+       
+
+       <div class="row" >
+          <div class = "col-sm-12">
+              <label for="idRol" >Rol de Usuario</label>
+              <select name="idRol" id="idRol" class="form-control" required>
+                <option value="" ></option>
+                <option value="1" >Cliente</option>
+                <option value="2" >Administrador</option>
+              </select>
+            </div>
+       </div>
+       <div class="row">
+       <div class = "col-sm-12">
+       <label for="ubicacion">Ubicacion</label>
+         <textarea name="ubicacion" id="ubicacion" class="form-control" required ></textarea>
         </div>
-        <div class="modal-body">
-          <p>Some text in the modal.</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
         </div>
       </div>
-      
+      <div class="modal-footer">
+        <span  class="btn btn-secondary" data-dismiss="modal">Cerrar</span>
+        <button  class="btn btn-primary">Guardar</button>
+      </div>
     </div>
   </div>
-  
 </div>
 
-</body>
-</html>
+</form>
